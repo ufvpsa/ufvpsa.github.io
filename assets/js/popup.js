a781bd8f74649c25c76b85027bfe8430ef39f440
Ziana <zkam215@hotmail.com>
2021-03-01 13:59:41 -0800

    Added pop-up to events page

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,24 +1,23 @@
 // Modal Pop-up Script
 
 var modal = document.getElementById("myModal");
-
 var btn = document.getElementById("myBtn");
-
 var span = document.getElementsByClassName("close")[0];
 
-
 window.onload = function() {
-  modal.style.display = "block";
+  if(sessionStorage.getItem('popState') != 'shown'){
+        modal.style.display = "block";
+        sessionStorage.setItem('popState','shown')
+    }
 }
-
 
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
